090276bd53026c24f24435ad0e1f20fb64219065
Heerko <heerko@swummoq.net>
2025-01-29 22:07:16 +0100

    v.01

--- seamfulness_1.54.ino
+++ seamfulness_1.54.ino
@@ -3,10 +3,12 @@
 #include "WebServerUtils.h"
 #include <GxEPD2_BW.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeSerif18pt7b.h>
 #include "ESPNowUtils.h"
 #include "FileSystemUtils.h"
 #include <ArduinoJson.h>
 #include </Users/hrko/_DEV/Arduino/TTGO_T5_Epaper/libraries/QRCode/src/qrcode.h>
+#include </Users/hrko/_DEV/Arduino/TTGO_T5_Epaper/libraries/QRCode/src/qrcode.c>
 #include "ConfigClass.h"
 
 // E-Ink Display Configuration
@@ -16,6 +18,9 @@
 #define EPD_BUSY 4
 
 GxEPD2_BW<GxEPD2_213_BN, GxEPD2_213_BN::HEIGHT> display(GxEPD2_213_BN(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
+QRCode qrcode;
+// uint8_t qrcodeData[128];  // Buffer for QR code data (adjust size based on version)
+
 
 char g_ssid[32] = "ESP_AP";  // set AP SSID. Can be overwritten by creating a file on LittleFS with extension .ssid
 
@@ -32,13 +37,35 @@ const char localIPURL[] = "http://4.3.2.1";
 const int WIFI_CHANNEL = 1;
 
 Config config;
+
+
+void setDisplayText(String header, String text) {
   display.setFullWindow();
-  display.setFont(&FreeMonoBold9pt7b);
-  display.setTextColor(GxEPD_BLACK);  // Tekstkleur
+  display.setTextColor(GxEPD_BLACK);
   display.firstPage();
+
+  int x = 0;
+  int y = 30;
+
   do {
-    display.fillScreen(GxEPD_WHITE);  // Achtergrondkleur
-    display.setCursor(10, 30);        // Positie van de tekst
+    display.fillScreen(GxEPD_WHITE);
+
+    // Print header
+    display.setFont(&FreeMonoBold9pt7b);
+    display.setCursor(x, y);
+    display.print(header);
+
+    // Get height of header text
+    int16_t tbx, tby;
+    uint16_t tbw, tbh;
+    display.getTextBounds(header, x, y, &tbx, &tby, &tbw, &tbh);
+
+    // Move down by text height + padding
+    y += tbh + 10;
+
+    // Print body text
+    display.setFont(&FreeSerif18pt7b);
+    display.setCursor(x, y);
     display.print(text);
   } while (display.nextPage());
 }
@@ -55,7 +82,7 @@ void handleMessageEndpoint(AsyncWebServerRequest *request) {
   }
 
   // String response = getMessagesFromFile();
-  
+
   request->send(200, "text/plain", response);
 }
 
@@ -70,7 +97,11 @@ void handleMessagesTxtEndpoint(AsyncWebServerRequest *request) {
 void handleUpdateDisplayEndpoint(AsyncWebServerRequest *request) {
   if (request->hasParam("text", true)) {
     String text = request->getParam("text", true)->value();
-    setDisplayText(text);
+    String q = "";
+    if (request->hasParam("question", true)) {
+      q = request->getParam("question", true)->value();
+    }
+    setDisplayText(q, text);
     saveMessageToFile(text);
     sendData(text);
     request->send(200, "text/plain", "Display updated with: " + text);
@@ -86,33 +117,63 @@ void customEndPoints() {
 }
 
 
-  String wifiData = "WIFI:S:" + config.ssid + ";T:nopass;;";
-    display.fillScreen(GxEPD_WHITE);
-    display.setCursor(x, y);
-    display.print("Hello World!");
-  } while (display.nextPage());
+void drawQRCode(QRCode *qrcode) {
+  int qrSize = qrcode->size;
+  int pixelSize = 4;  // Adjust based on your display
+  int xOffset = (display.width() - (qrSize * pixelSize)) / 2;
+  int yOffset = (display.height() - (qrSize * pixelSize)) / 2;
+
+  for (int y = 0; y < qrSize; y++) {
+    for (int x = 0; x < qrSize; x++) {
+      if (qrcode_getModule(qrcode, x, y)) {
+        display.fillRect(
+          xOffset + x * pixelSize,
+          yOffset + y * pixelSize,
+          pixelSize,
+          pixelSize,
+          GxEPD_BLACK);
+      }
+    }
+  }
 }
 
+void showQRCode() {
+  int bufferSize = qrcode_getBufferSize(3);
+  uint8_t *qrcodeData = (uint8_t *)malloc(bufferSize);  // Dynamically allocate memory
+
+  String wifiData = "WIFI:S:" + config.ssid + ";T:nopass;;";
+  qrcode_initText(&qrcode, qrcodeData, 3, ECC_LOW, wifiData.c_str());
+
+  display.fillScreen(GxEPD_WHITE);
+  drawQRCode(&qrcode);
+
+  display.display();
+}
 
 void setup() {
   Serial.begin(115200);
-  if (!LittleFS.begin()) {
-    Serial.println("Failed to mount LittleFS");
+  if (!initFileSystem()) {
     return;
   }
+  listFiles();
 
   display.init(115200);
   display.setRotation(1);
   display.setFont(&FreeMonoBold9pt7b);
-  
+
   config.load();  // Load configuration
+  strncpy(g_ssid, config.ssid.c_str(), sizeof(g_ssid) - 1);
+  g_ssid[sizeof(g_ssid) - 1] = '\0';
 
   if (config.role == "ap") {
-  startSoftAccessPoint(g_ssid, NULL, localIP, gatewayIP);
+    startSoftAccessPoint(g_ssid, NULL, localIP, gatewayIP);
   } else {
     Serial.println("Starting WiFi in STA Mode");
     WiFi.mode(WIFI_STA);
   }
+  Serial.printf("RSSI: %d dBm\n", WiFi.RSSI());
+  Serial.printf("Wi-Fi channel: %d\n", WiFi.channel());
+
   setUpDNSServer(dnsServer, localIP);
   setUpWebserver(server, localIP);
   customEndPoints();
